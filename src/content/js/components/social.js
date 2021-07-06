d060bc27ee8aa2802a6a1ede7aa7207c0ac24494
BMscis <melvinwafula@gmail.com>
2021-07-06 23:05:02 +0300

    fb test 5

--- src/content/js/components/social.js
+++ src/content/js/components/social.js
@@ -43,7 +43,7 @@ export class Social extends HTMLElement {
         this.appendChild(this.components.socialpad)
         this.appendChild(this.components.facebookhref)
         this.components.facebookhref.outerHTML = `
-        <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FLightAcademyAlumniPresident&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=843006239656820" width="500" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+        <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FLightAcademyAlumni&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=843006239656820" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
         `
         return
     }
