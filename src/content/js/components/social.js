3f0779e8d334e2ebb201658012d6556308eb425f
BMscis <melvinwafula@gmail.com>
2021-07-07 14:42:03 +0300

    instagram test

--- src/content/js/components/social.js
+++ src/content/js/components/social.js
@@ -13,7 +13,7 @@ export class Social extends HTMLElement {
         let twitterScript = document.createElement("script")
         twitterScript.setAttribute("async", "")
         twitterScript.src = "https://platform.twitter.com/widgets.js"
-        //document.body.appendChild(twitterScript)
+        document.body.appendChild(twitterScript)
         this.components = this.getComponents
         return
     }
@@ -23,10 +23,11 @@ export class Social extends HTMLElement {
         twitterHref.classList.add("twitter-timeline")
         twitterHref.href = "https://twitter.com/LightAlumni?ref_src=twsrc%5Etfw"
         twitterHref.innerHTML = "Tweets by LightAlumni"
-
+        twitterHref.classList.add("twitteriframe")
         //facebookHref
         //
         let facebookHref = document.createElement("iframe")
+        facebookHref.classList.add("facebookiframe")
         
         return {
             "socialpad": new SocialPad(),
@@ -41,9 +42,11 @@ export class Social extends HTMLElement {
     }
     render() {
         this.appendChild(this.components.socialpad)
+        this.appendChild(this.components.twitterhref)
         this.appendChild(this.components.facebookhref)
         this.components.facebookhref.outerHTML = `
-        <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FLightAcademyAlumni&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=843006239656820" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+        <iframe class=facebookiframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FLightAcademyAlumni&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=843006239656820" width="100%" height="100%" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+        <iframe class=instagramiframe src="https://www.instagram.com/lightalumni/EAALZBta91S3QBANNBpwZB9gEKbx7jbbZCgf8DZCwu5SfonlQmm46xvtCI4V3wm8yqOSxNzighjQi9vWst5ZCrltZCkCDap5GDy1sY6VNzPmyuManiJdofn8v47Wxd4Ys2mQOXdalqoZB10wrBh7yruumcbPRRZChCjFJLH7dkJZA8bMcw1NBZASqWwss6Etx1ZCfdezoCD478fBbwZDZD"></iframe>
         `
         return
     }
